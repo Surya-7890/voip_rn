ad3fad2576ca6d13ccda153f25abdd8c5e5fd145
Sri Harish <sriharish7635@gmail.com>
2024-03-01 21:54:12 +0530

    initial commit

--- android/app/src/main/java/com/voip_rn/MainActivity.kt
+++ android/app/src/main/java/com/voip_rn/MainActivity.kt
@@ -4,8 +4,24 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.linphone.core.*
+import android.os.Bundle
+import com.facebook.react.bridge.ReactContext
+
+
 
 class MainActivity : ReactActivity() {
+  private  var core: Core?=null
+
+  override fun onCreate(savedInstanceState: Bundle?) {    
+    super.onCreate(savedInstanceState)
+    val factory = Factory.instance()
+    core=factory.createCore(null,null,this)    
+  }
+
+  fun getCoreInstance(): Core?{
+    return core
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,9 +1,15 @@
-import { Button, Text, View, TouchableNativeFeedback, TouchableHighlight, TouchableOpacity, Platform, Image } from "react-native";
+import { Button, Text, View, TouchableNativeFeedback, TouchableHighlight, TouchableOpacity, Platform, Image, NativeModules } from "react-native";
 import GoogleIcon from "../public/google_logo.png";
+import { useEffect } from "react";
 
 let PlatformSpecificComponent = Platform.OS === 'android' ? TouchableNativeFeedback : Platform.OS === 'ios' ? TouchableHighlight : TouchableOpacity
-
+const  { Test } = NativeModules;
 export default function Login({ setAuth }) {
+
+  useEffect(() => {
+    Test.test("7002","7002","192.168.57.227", (name) => console.log(name))
+  }, [])
+
   return (
     <View style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <View style={{ height: '35%', width: '80%', display: 'flex', justifyContent: 'space-evenly' }}>
