3424a1d2bffcae92e69dbbff44f7d42a6de5885b
Sri Harish <sriharish7635@gmail.com>
2024-03-02 10:53:31 +0530

    login logic(authentication)

--- android/app/src/main/java/com/voip_rn/Test.kt
+++ android/app/src/main/java/com/voip_rn/Test.kt
@@ -14,6 +14,7 @@ import android.Manifest
 class Test(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "Test"
 
+    private var core: Core? = null
 
     @ReactMethod
     fun test(username: String, password: String, domain: String, cb: Callback) {  
@@ -21,10 +22,20 @@ class Test(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(r
         try{
 
             val mainActivity = currentActivity as MainActivity?
-            val core = mainActivity?.getCoreInstance()
+            core = mainActivity?.getCoreInstance()
 
             core?.let { safeCore ->
 
+                val coreListener = object : CoreListenerStub() {
+                    override fun onAccountRegistrationStateChanged(core: Core, account: Account, state: RegistrationState, message: String) {
+
+                        if (state == RegistrationState.Failed ) {                            
+                            cb.invoke(state.toString())
+                        } else if (state == RegistrationState.Ok) {
+                            cb.invoke(state.toString())                            
+                        }
+                    }
+                }
                 val transportType = TransportType.Udp
                 val authInfo = Factory.instance().createAuthInfo(username, null, password, null, null, domain, null)
                 val params = safeCore.createAccountParams()
@@ -33,20 +44,19 @@ class Test(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(r
                 val address = Factory.instance().createAddress("sip:$domain")
                 
                 address?.transport = transportType
-                params.serverAddress=address
+                params.serverAddress=address                
 
                 val account = safeCore.createAccount(params)
                 safeCore.addAuthInfo(authInfo)
                 safeCore.addAccount(account)
 
                 safeCore.defaultAccount = account
-                safeCore.start()
-                cb.invoke("Sip conf")              
+                safeCore.addListener(coreListener)                
+                safeCore.start()         
                 
             } ?: run {
                 cb.invoke("err")
-            }
-            
+            }            
            
         }catch(e:Exception){
             cb.invoke(e)
